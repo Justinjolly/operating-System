76358bcafda8d4a92614a56671ed0e96ff3137a5
Justin Jolly <83817846+Justinjolly@users.noreply.github.com>
2022-10-04 11:35:30 +0530

    opendir(),closedir(),readdir()

---
+++ call.c
@@ -0,0 +1,21 @@
+//opendir(),readdir(),ciose()
+#include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
+struct dirent *dptr;
+int main(){
+char buff[256];
+DIR*dirp;
+printf("Enter the directory name: ");
+scanf("%s",buff);
+if((dirp=opendir(buff))==NULL){
+printf("Error");
+exit(1);
+}
+while(dptr=readdir(dirp)){
+printf("%s\n",dptr->d_name);
+}
+closedir(dirp);
+return 0;
+}
+
